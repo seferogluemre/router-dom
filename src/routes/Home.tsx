53d1707120e54a35643f15e1d0d2f7b6bbf01785
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-28 23:09:47 +0300

    bsi indeksi hesaplama ve result döndürme

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,10 +8,19 @@ export default function HomePage() {
 
     const firstName = UserState((state) => state.firstName);
     const lastName = UserState((state) => state.lastName);
+    const birthDate = UserState((state) => state.birthDate);
+    const kg = UserState((state) => state.weight);
+    const cm = UserState((state) => state.height);
+
     const age = UserState((state) => state.getAge())
+    const bmi = UserState((state) => state.getBmi())
+    const getBmiResult = UserState((state) => state.getBmiResult())
+
     const setFirstName = UserState((state) => state.setFirstName);
     const setLastName = UserState((state) => state.setLastName);
     const setBirthDate = UserState((state) => state.setBirthDate);
+    const setHeight = UserState((state) => state.setHeight);
+    const setWeight = UserState((state) => state.setWeight);
 
     function handleChangeFirstname(e: ChangeEvent<HTMLInputElement>) {
         setFirstName(e.target.value)
@@ -20,8 +29,13 @@ export default function HomePage() {
         setLastName(e.target.value)
     }
     function handleChangeBirtDate(e: ChangeEvent<HTMLInputElement>) {
-        const birthDatePayload = new Date(e.target.value);
-        setBirthDate(birthDatePayload)
+        setBirthDate(e.target.valueAsDate!)
+    }
+    function handleChangeHeight(e: ChangeEvent<HTMLInputElement>) {
+        setHeight(e.target.valueAsNumber || undefined)
+    }
+    function handleChangeWeight(e: ChangeEvent<HTMLInputElement>) {
+        setWeight(e.target.valueAsNumber || undefined)
     }
 
     return (
@@ -34,24 +48,43 @@ export default function HomePage() {
                 İsim:{firstName}
             </div>
             <div style={{ margin: "50px" }}>
-                İsim:{lastName}
+                Soyİsim:{lastName}
             </div>
             <div style={{ margin: "50px" }}>
                 Yaş:{age}
-            </div>  
+            </div>
+            <div style={{ margin: "50px" }}>
+                Boy:{cm}
+            </div>
+            <div style={{ margin: "50px" }}>
+                Kilo:{kg}
+            </div>
+            <div style={{ margin: "50px" }}>
+                Vicut Kitle Endeksi:{bmi} & {getBmiResult}
+            </div>
+
+
             <div style={{ margin: "100px" }}>
                 <Form>
                     <div className="mb-4">
                         <label >İsim</label>
-                        <FormControl onChange={handleChangeFirstname} placeholder="İsminizi giriniz..." className="w-25" type="text" />
+                        <FormControl onChange={handleChangeFirstname} value={firstName} placeholder="İsminizi giriniz..." className="w-25" type="text" />
                     </div>
                     <div className="mb-4">
                         <label >Soyisim</label>
-                        <FormControl onChange={handleChangeLastName} placeholder="Soyisiminizi giriniz..." className="w-25" type="text" />
+                        <FormControl onChange={handleChangeLastName} value={lastName} placeholder="Soyisiminizi giriniz..." className="w-25" type="text" />
                     </div>
                     <div className="mb-4">
                         <label >Dogum Tarihiniz</label>
-                        <FormControl onChange={handleChangeBirtDate} className="w-25" type="date" />
+                        <FormControl onChange={handleChangeBirtDate} value={birthDate?.toLocaleDateString("en-CA")} className="w-25" type="date" />
+                    </div>
+                    <div className="mb-4">
+                        <label >Boy </label>
+                        <FormControl onChange={handleChangeHeight} value={cm} className="w-25" type="number" />
+                    </div>
+                    <div className="mb-4">
+                        <label >Kilo </label>
+                        <FormControl onChange={handleChangeWeight} value={kg} className="w-25" type="number" />
                     </div>
                 </Form>
             </div>
--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -9,8 +9,16 @@ interface UserState {
     setFirstName: (payload:string)=>void;
     setLastName: (payload:string)=>void;
     setBirthDate: (payload:Date)=>void;
+    setHeight: (payload:number | undefined)=>void;
+    setWeight: (payload:number | undefined)=>void;
     getAge:()=>number|undefined;
+    getBmi:()=>number;
+    getBmiResult:()=>"Zayıf" | "Normal" |"Kilolu" | "Obez";
 }
+
+const calculateBmiScore=(weight:number,height:number)=>(weight / ((height*height)/10000)).toFixed(2);
+
+
 
 export const UserState = create<UserState>()((set,get) => ({
     firstName:"",
@@ -18,12 +26,32 @@ export const UserState = create<UserState>()((set,get) => ({
     setFirstName: (payload) => set({ firstName: payload }),
     setLastName: (payload) => set({ lastName: payload }),
     setBirthDate: (payload)=>set({ birthDate: payload }),
+    setHeight: (payload)=>set({ height: payload }),
+    setWeight: (payload)=>set({ weight: payload }),
+
     getAge:()=>{
         const birthDate=get().birthDate;
-        if(!birthDate)return;
+        if(!birthDate)return;   
         const ageDifmis=Date.now() - birthDate.getTime();
         const ageDate=new Date(ageDifmis);
         return Math.abs(ageDate.getUTCFullYear() - 1970)
-    }
+    },
+    
+    getBmi:()=>{
+        const weight=get().weight;
+        const height=get().height;
+        
+        if(!height || !weight) return 0;
+    
+        const score= +calculateBmiScore(weight,height);
+        return score;
+    },
+    getBmiResult:()=> {
+        const getBmi=get().getBmi();
+        if(getBmi<18.5)return "Zayıf";
+        if(getBmi<25)return "Normal"
+        if(getBmi<30)return "Kilolu"
 
+        return "Obez";
+    },
 }))
